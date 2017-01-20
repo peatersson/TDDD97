1470f20944a5058a1e073da1f870a96f7e5954ce
Niklas Pettersson <nikpe353@student.liu.se>
2017-01-20 19:47:55 +0100

    feat(loading of page/looks): fixed some things

---
+++ client.js
@@ -0,0 +1,11 @@
+setBody = function(view){
+  document.getElementById("body").innerHTML = view.innerHTML;
+};
+
+
+window.onload = function(){
+  welcomeView = document.getElementById("welcomeView");
+  profileView = document.getElementById("profileView");
+
+  setBody(welcomeView);
+};

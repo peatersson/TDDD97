29488a287bba1ea0ea94b0f4e481ef3cc8114c9f
Niklas Pettersson <nikpe353@student.liu.se>
2017-01-24 12:01:28 +0100

    feat(lab1): done with tasks in lab1

--- client.js
+++ client.js
@@ -84,9 +84,9 @@ function signOut(){
 
 function loadInfo(){
     var returnCode = serverstub.getUserDataByToken(userToken);
-    var user = returnCode.data;
 
     if(returnCode.success){
+      var user = returnCode.data;
       document.getElementById("nameLabel").innerHTML = "Name: " + user.firstname + " " + user.familyname;
       document.getElementById("genderLabel").innerHTML = "Gender: " + user.gender;
       document.getElementById("cityLabel").innerHTML = "City: " + user.city;
@@ -109,9 +109,55 @@ function loadHomeMessages(){
   }
 }
 
-function postMessage(){
-  var errorArea = document.getElementById("postMessageError");
-  var returnCode = serverstub.postMessage(userToken, document.getElementById("postBox").value, userEmail);
-
+function postMessage(toEmail){
+  var errorArea = null;
+  var input = null;
+  if(toEmail == null){
+    toEmail = userEmail;
+    errorArea = document.getElementById("postMessageError");
+    input = document.getElementById("postBox").value;
+  }else{
+    errorArea = document.getElementById("browsePostMessageError");
+    input = document.getElementById("browsePostBox").value
+  }
+  var returnCode = serverstub.postMessage(userToken, input, toEmail);
   errorArea.innerHTML = returnCode.message;
+  toEmail = null;
+}
+
+var searchedUser = null;
+function searchUser(){
+  email = document.getElementById("searchField").value;
+  var returnCode = serverstub.getUserDataByEmail(userToken, email);
+  var errorArea = document.getElementById("searchUserError");
+
+  if(returnCode.success){
+    searchedUser = email;
+    var user = returnCode.data;
+    document.getElementById("browseNameLabel").innerHTML = "Name: " + user.firstname + " " + user.familyname;
+    document.getElementById("browseGenderLabel").innerHTML = "Gender: " + user.gender;
+    document.getElementById("browseCityLabel").innerHTML = "City: " + user.city;
+    document.getElementById("browseCountryLabel").innerHTML = "Country: " + user.country;
+    document.getElementById("browseEmailLabel").innerHTML = "Email: " + user.email;
+    document.getElementById("browseHeadingUser").innerHTML += user.firstname;
+    loadBrowseMessages();
+  }
+  errorArea.innerHTML = returnCode.message;
+}
+
+function loadBrowseMessages(){
+  var returnCode = serverstub.getUserMessagesByEmail(userToken, searchedUser);
+
+  if(returnCode.success){
+    var messages = returnCode.data;
+    document.getElementById("browseMessageWall").innerHTML = null;
+    for (i = 0; i < messages.length; i++) {
+      document.getElementById("browseMessageWall").innerHTML += "<p>From: " + messages[i].writer + "<br>";
+      document.getElementById("browseMessageWall").innerHTML += messages[i].content + "<br></p>";
+    }
+  }
+}
+
+function postMessageToUser(){
+  postMessage(searchedUser);
 }
